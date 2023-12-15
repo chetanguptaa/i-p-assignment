021f99baf8764dbd2d32f8d3c5d1d46f46160c5c
chetan gupta <chetangupta123raj@gmail.com>
2023-12-15 14:42:33 +0530

    updated third answer

--- third/client/src/pages/ResultPage.tsx
+++ third/client/src/pages/ResultPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 type Result = {
@@ -28,7 +28,6 @@ const ResultPage = () => {
     fetchData();
   }, []);
 
-  // Calculate the indexes for the current page
   const indexOfLastResult = currentPage * resultsPerPage;
   const indexOfFirstResult = indexOfLastResult - resultsPerPage;
   const currentResults = results.slice(indexOfFirstResult, indexOfLastResult);
@@ -36,7 +35,7 @@ const ResultPage = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="flex-col">
-        <table className="shadow-lg ">
+        <table className="shadow-lg">
           <thead>
             <tr>
               <th className="bg-slate-700 text-left px-8 py-4 text-white rounded-l-md">
@@ -56,8 +55,7 @@ const ResultPage = () => {
             ))}
           </tbody>
         </table>
-        <div className="mt-4 flex justify-between">
-          {/* Add pagination controls */}
+        <div className="mt-4 flex justify-center">
           <button
             onClick={() => setCurrentPage((prev) => prev - 1)}
             disabled={currentPage === 1}
@@ -65,7 +63,7 @@ const ResultPage = () => {
           >
             Previous
           </button>
-          <span className="mx-2 bg-slate-800 p-1 rounded text-white w-20 text-center">
+          <span className="mx-2  p-1 rounded text-black w-20 text-center">
             Page {currentPage}
           </span>
           <button
